33558600db04fb8d47c4f57d836e290a7c32449c
GoryMoon <gurreja@gmail.com>
2017-11-23 19:40:35 +0100

    Changed structure

---
+++ ARBoardgame/Assets/Scripts/Common/PiecePutMSG.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePutMSG
+{
+    public string PieceType { get; private set; }
+    public SquarePos SquarePos { get; private set; }
+
+    private PiecePutMSG() { }
+
+    public PiecePutMSG(string pieceType, SquarePos squarePos)
+    {
+        PieceType = pieceType;
+        SquarePos = squarePos;
+    }
+}
---
+++ ARBoardgame/Assets/Scripts/Controller/InteractionController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public bool BoardPositioned { get; private set; }
+
+    public const string LMB_CLICK = "LMB click";
+    public const string RMB_CLICK = "RMB click";
+    public const string BOARD_POSITIONED = "Board position selected";
+    private int count;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        BoardPositioned = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleLMBClick();
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            HandleRMBClick();
+        }
+    }
+
+    public void HandleLMBClick()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (BoardPositioned == false)
+        {
+            this.PostNotification(BOARD_POSITIONED, cursorPos);
+
+            BoardPositioned = true;
+        }
+        else
+        {
+            this.PostNotification(LMB_CLICK, cursorPos);
+        }
+
+    }
+
+    public void HandleRMBClick()
+    {
+        if (BoardPositioned == true)
+        {
+            Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            this.PostNotification(RMB_CLICK, cursorPos);
+        }
+    }
+}
