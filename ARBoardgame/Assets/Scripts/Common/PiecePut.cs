fa4ed136dbcd44b73bddad752237589ac82d8f18
magnlar <cmr.larsson@gmail.com>
2017-11-24 15:48:57 +0100

    PiecePutMSG is PiecePut instead

---
+++ ARBoardgame/Assets/Scripts/Common/PiecePut.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecePut
+{
+    public string PieceType { get; private set; }
+    public SquarePos SquarePos { get; private set; }
+
+    private PiecePut() { }
+
+    public PiecePut(string pieceType, SquarePos squarePos)
+    {
+        PieceType = pieceType;
+        SquarePos = squarePos;
+    }
+}
