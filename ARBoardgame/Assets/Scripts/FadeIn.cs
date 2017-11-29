48eb3a50aea5c18f2b810f20f48adc74af03db84
Dennis Ek <dennis.a.ek@gmail.com>
2017-11-29 16:24:49 +0100

    Fixed a problem where disabled menu buttons weren't greyed out.

--- ARBoardgame/Assets/Scripts/FadeIn.cs
+++ ARBoardgame/Assets/Scripts/FadeIn.cs
@@ -11,7 +11,12 @@ public class FadeIn : MonoBehaviour {
 		GetComponent<CanvasRenderer>().SetAlpha(0f);
 		// During 1 second, fade to alpha 1 (zero transparency)
 		if(GetComponent<Graphic>() != null) {
-			GetComponent<Graphic>().CrossFadeAlpha(1f, 1f, false);
+			float fadeTarget = 1f;
+			// Special case when we're dealing with disabled buttons
+			if(GetComponent<Button>() && !GetComponent<Button>().interactable) {
+				fadeTarget = 0.5f;
+			}
+			GetComponent<Graphic>().CrossFadeAlpha(fadeTarget, 1f, false);
 		}
 		// Destroy this script after 1 seconds, it has served its purpose by then
 		Object.Destroy(this, 1);
