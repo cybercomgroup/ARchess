4105ba3edb6f0094c81ed416948c8c015b413198
zoizer <zoizer123@hotmail.com>
2017-12-04 16:30:54 +0100

    Refactored Network Code

---
+++ ARBoardgame/Assets/Controller/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkController : NetworkManager {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void StartAsHost()
+    {
+        StartHost();
+    }
+
+    void StartAsClient()
+    {
+        StartClient(); // missing ip, default localhost?
+    }
+}
