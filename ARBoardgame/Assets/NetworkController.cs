ae6eb80f2c4569435c1f55d3d676a4bf310d1dc8
zoizer <zoizer123@hotmail.com>
2017-12-07 18:05:48 +0100

    NetworkDemo Pretty much done

---
+++ ARBoardgame/Assets/NetworkController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkController : MonoBehaviour {
+    public NetworkManager net;
+    private bool busy;
+
+	// Use this for initialization
+	void Start () {
+        busy = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.H) && !busy)
+        {
+            busy = true;
+            net.StartHost();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J) && !busy)
+        {
+            busy = true;
+            net.StartClient();
+        }
+    }
+}
